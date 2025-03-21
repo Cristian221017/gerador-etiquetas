418fcf8efd4cdc7a3760299424badb2af0832d12
Cristian <crisrd2608@gmail.com>
2025-03-21 20:48:58 -0300

    Corrigindo rota

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import io
 
 app = Flask(__name__)
 
-@app.route("/gerar_etiqueta", methods=["POST"])  # CERTIFIQUE-SE DE QUE ESSA LINHA ESTÁ NO CÓDIGO
+@app.route("/gerar_etiqueta", methods=["POST"])
 def gerar_etiqueta():
     data = request.json
     remetente = data.get("remetente", "Remetente Padrão")
